d5665326b223b7531647748a817f52be12971f7b
spaguettibrain <38581528+spaguettibrain@users.noreply.github.com>
2018-04-22 14:41:17 +0200

    tunnel

--- map/main.tsx
+++ map/main.tsx
@@ -1,4 +1,24 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="main" tilewidth="16" tileheight="16" tilecount="100" columns="10">
- <image source="tilesheet.png" width="160" height="160"/>
+<tileset name="main" tilewidth="16" tileheight="16" tilecount="400" columns="20">
+ <image source="tilesheet.png" width="320" height="320"/>
+ <tile id="94">
+  <animation>
+   <frame tileid="74" duration="200"/>
+   <frame tileid="75" duration="200"/>
+   <frame tileid="76" duration="200"/>
+   <frame tileid="77" duration="200"/>
+   <frame tileid="76" duration="200"/>
+   <frame tileid="75" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="95">
+  <animation>
+   <frame tileid="75" duration="200"/>
+   <frame tileid="76" duration="100"/>
+   <frame tileid="77" duration="200"/>
+   <frame tileid="76" duration="200"/>
+   <frame tileid="75" duration="200"/>
+   <frame tileid="74" duration="200"/>
+  </animation>
+ </tile>
 </tileset>

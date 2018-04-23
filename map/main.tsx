33e9115a40cf51d034f1cdc49e5d50fba88197af
spaguettibrain <38581528+spaguettibrain@users.noreply.github.com>
2018-04-23 16:27:54 +0200

    leuchtturm animation

--- map/main.tsx
+++ map/main.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="main" tilewidth="16" tileheight="16" tilecount="400" columns="20">
- <image source="tilesheet.png" width="320" height="320"/>
+<tileset name="main" tilewidth="16" tileheight="16" tilecount="1240" columns="20">
+ <image source="tilesheet.png" width="320" height="500"/>
  <tile id="94">
   <animation>
    <frame tileid="74" duration="200"/>
@@ -19,6 +19,30 @@
    <frame tileid="76" duration="200"/>
    <frame tileid="75" duration="200"/>
    <frame tileid="74" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="430">
+  <animation>
+   <frame tileid="434" duration="400"/>
+   <frame tileid="474" duration="300"/>
+   <frame tileid="514" duration="200"/>
+   <frame tileid="474" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="450">
+  <animation>
+   <frame tileid="436" duration="400"/>
+   <frame tileid="476" duration="300"/>
+   <frame tileid="516" duration="200"/>
+   <frame tileid="476" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="470">
+  <animation>
+   <frame tileid="435" duration="400"/>
+   <frame tileid="475" duration="300"/>
+   <frame tileid="515" duration="200"/>
+   <frame tileid="475" duration="100"/>
   </animation>
  </tile>
 </tileset>
